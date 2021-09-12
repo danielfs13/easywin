332820b5a58c25f429ff90eaef03eb37d4e42835
Daniel Strano <88854271+danielfs13@users.noreply.github.com>
2021-09-11 23:23:53 -0300

    Add files via upload
    estrutura  básica da página home finalizada, falta estilização.

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+const usericon = document.getElementById("usericon");
+
+function showlogin () {
+  /*  document.getElementsByClassName('usericon').item. */ 
+  console.log('foi')
+
+}
+
+/* usericon.addEventListener('click', function() {
+    console.log('foi') 
+}) */
